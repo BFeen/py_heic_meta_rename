4c0585812c962629714684308093853a77cef6a6
Borislaw <bfeen@mail.ru>
2023-08-06 17:08:54 +0300

    Update main.py
    Now we can handle .heic, .jpg and exceptions!

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from pathlib import Path
 from wand.image import Image
 
-path = Path(__file__).parent
+source_folder = Path('img/')
 
 # PATH + WAND
 names = []
-length = len(list(enumerate(path.glob('*.heic'))))
+length = len(list(enumerate(source_folder.glob('*.*'))))
 
-for i, path in enumerate(path.glob('*.heic')):
+for i, path in enumerate(source_folder.glob('*.*')):
     create_date = ''
-
-    with Image(filename=path.name) as file:
-        create_date = file.metadata["exif:DateTime"]
-        create_date = create_date.replace(":", ".")
-
-    #составление нового имени файла
-    new_name = create_date + path.suffix
-    #проверка на уникальность имени файла
+# Extracting metadata with creation date from file
+    with Image(filename=path) as file:
+        try:    
+            create_date = file.metadata["exif:DateTime"]
+            create_date = create_date.replace(":", ".")
+        except Exception:
+            print(f'Файл {path} не имеет сведений о дате создания')
+            continue
+# Creating new file name
+    new_name = "img/" + create_date + path.suffix
+    print(new_name)
+# Check for unique file name
     if new_name in names:
         new_name = create_date + '_1' + path.suffix
     
     names.append(new_name)
     
-    # полезная информация
+# Additional info
     length -= 1
     print(f'{new_name} - done, осталось переименовать: {length} файлов')
     path.rename(new_name)
